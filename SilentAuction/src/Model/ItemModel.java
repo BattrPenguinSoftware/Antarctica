65401317df4ba27274612ee9cebf5c4e6f77303d
AudreyC <horus1228@yahoo.com>
2015-05-21 20:37:54 -0700

    Need help with implementing the methods

---
+++ SilentAuction/src/Model/ItemModel.java
@@ -0,0 +1,81 @@
+package Model;
+
+
+
+/**
+ * The Class ItemModel, represents an item.
+ * @author Audrey Chavarria 5/21/15
+ *  
+ */
+public class ItemModel {
+	
+	/** The image associated witht the item. */
+	private File image;
+	
+	/** The name of the item. */
+	private String name;
+	
+	/** The donor who donated the item. */
+	private DonorModel donor;
+	
+	/** The bid history of the item. */
+	private Bid[] bidHistory;
+	
+	/** The item number that uniquely identifies it. */
+	private int itemNumber;
+	
+	
+	/**
+	 *  Constructor for the item.
+	 *
+	 * @param theName of the item
+	 * @param theDonor of the item
+	 * @param theItemNumber that uniquely identifies it
+	 */
+	public ItemModel(String theName, DonorModel theDonor, int theItemNumber){
+		name = theName;
+		donor = theDonor;
+		bidHistory = {null};
+		itemNumber = theItemNumber;	
+	}	
+	
+	/**
+	 * Gets the detials.
+	 *
+	 * @return the detials
+	 */
+	public void getDetials( ) {
+	}
+	
+	/**
+	 * Verify new bid.
+	 *
+	 * @param NewBid the new bid
+	 * @return true, if successful
+	 */
+	private boolean verifyNewBid(double NewBid ){
+	}
+	
+	/**
+	 * Gets the max bid.
+	 *
+	 * @return the max bid
+	 */
+	public Bid getMaxBid(){		
+	}
+	
+	/**
+	 * Saveinfo.
+	 */
+	private void saveinfo(){		
+	}
+	
+	/**
+	 * Gets the bid history.
+	 *
+	 * @return the bid history
+	 */
+	public void getBidHistory(){		
+	}
+
+}
