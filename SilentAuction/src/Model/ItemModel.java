9f4eaf215aa6474d828a731e20cbecd50d27fb71
AudreyC <horus1228@yahoo.com>
2015-05-26 15:53:56 -0700

    Updated class

--- SilentAuction/src/Model/ItemModel.java
+++ SilentAuction/src/Model/ItemModel.java
@@ -1,78 +1,111 @@
 package Model;
+
+import java.awt.Image;
+import java.util.ArrayList;
+
 /**
  * The Class ItemModel, represents an item.
+ * 
  * @author Audrey Chavarria 5/21/15
- *  
+ * 
  */
 public class ItemModel {
-	
-	/** The image associated witht the item. */
-	private File image;
-	
+
+	/** The image associated with the item. */
+	private Image image;
+
 	/** The name of the item. */
 	private String name;
-	
+
 	/** The donor who donated the item. */
 	private Donor donor;
-	
+
 	/** The bid history of the item. */
-	private Bid[] bidHistory;
-	
+	private ArrayList<Bid> bidHistory;
+
 	/** The item number that uniquely identifies it. */
 	private int itemNumber;
-	
-	
+
+	private double currentBid;
+
 	/**
-	 *  Constructor for the item.
+	 * Constructor for the item.
 	 *
-	 * @param theName of the item
-	 * @param theDonor of the item
-	 * @param theItemNumber that uniquely identifies it
+	 * @param theName
+	 *            of the item
+	 * @param theDonor
+	 *            of the item
+	 * @param theItemNumber
+	 *            that uniquely identifies it
 	 */
-	public ItemModel(String theName, Donor theDonor, int theItemNumber){
+	public ItemModel(String theName, Donor theDonor, int theItemNumber,
+			Image theImage, double initialPrice) {
 		name = theName;
 		donor = theDonor;
-		bidHistory = {null};
-		itemNumber = theItemNumber;	
-	}	
-	
-	/**
-	 * Gets the detials.
-	 *
-	 * @return the detials
-	 */
-	public void getDetials( ) {
+		bidHistory = new ArrayList<Bid>();
+		itemNumber = theItemNumber;
+		image = theImage;
+		currentBid = initialPrice;
+
 	}
-	
+
 	/**
-	 * Verify new bid.
-	 *
-	 * @param NewBid the new bid
+	 * Verify new bid. somoneone try to place a new bid, verify checks current
+	 * bid price and says if its is new bidd is greater than current price
+	 * 
+	 * @param NewBid
+	 *            the new bid
 	 * @return true, if successful
 	 */
-	private boolean verifyNewBid(double NewBid ){
+	private boolean verifyNewBid(double newBid) {
+		return newBid > currentBid;
 	}
-	
+
+	// add to end of arryalsit, and update current highest bid
+	public void placeNewBid(Bid theNewBid) {
+		bidHistory.add(theNewBid);
+		currentBid = theNewBid.getAmount();
+
+	}
+
+	// ///////////////////////////
 	/**
-	 * Gets the max bid.
-	 *
-	 * @return the max bid
+	 * Saveinfo. worry about it later
 	 */
-	public Bid getMaxBid(){		
-	}
-	
+	private void saveinfo() {
+	}// ////////////////////////////
+
 	/**
-	 * Saveinfo.
-	 */
-	private void saveinfo(){		
-	}
-	
-	/**
-	 * Gets the bid history.
-	 *
+	 * Gets the bid history. return whole arraylist
+	 * 
 	 * @return the bid history
 	 */
-	public void getBidHistory(){		
+	public ArrayList<Bid> getBidHistory() {
+		return bidHistory;
+	}
+
+	/**
+	 * Gets the max bid. return latest bid obj from araylist
+	 * 
+	 * @return the max bid
+	 */
+	public Bid getMaxBid() {
+		return bidHistory.get(bidHistory.size() - 1);
+	}
+
+	public void getCurrentBid() {
+	}
+
+	public void getItemNumber() {
+	}
+
+	public void getImage() {
+	}
+
+	public void getName() {
+	}
+
+	public void getDonor() {
 	}
 
 }
