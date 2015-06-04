845274986ac4f553b93470a11c73ad48640f926f
Ben Cassidy <ben.cassidy11@gmail.com>
2015-06-03 17:53:31 -0700

    started AuctionTest

---
+++ SilentAuction/src/Model/AuctionTest.java
@@ -0,0 +1,43 @@
+package Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuctionTest {
+	
+	Auction test;
+	
+	ItemModel testItem;
+	
+	Donor testDoner;
+
+	@Before
+	public void setUp() throws Exception {
+		test = new Auction();
+		testItem = new ItemModel("testItem", testDoner, 0, null, 20.0, false);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAuction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddItem() {
+		test.addItem(testItem);
+		assertEquals()
+	}
+
+	@Test
+	public void testGetItems() {
+		fail("Not yet implemented");
+	}
+
+}
